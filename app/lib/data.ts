d68dba456a926f1d28760347930caedc4275f488
ko44d <koyoshid622@gmail.com>
2024-10-01 15:14:12 +0900

    チャートを表示する処理を追加

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,6 +8,9 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
 
 export async function fetchRevenue() {
   try {
@@ -17,11 +20,11 @@ export async function fetchRevenue() {
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
+    const data = await prisma.revenue.findMany();
 
     // console.log('Data fetch completed after 3 seconds.');
 
-    return data.rows;
+    return data;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
